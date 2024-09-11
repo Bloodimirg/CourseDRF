6a3a2edf2ebf40255bbc960a94649c207b517335
Bloodimirg <bloodimirg@gmail.com>
2024-09-11 12:48:25 +0300

    задан пароль, хост, порт к базе данных

--- config/settings.py
+++ config/settings.py
@@ -64,6 +64,9 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": os.getenv("DB_NAME"),
         "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        'HOST': os.getenv('HOST'),
+        'PORT': os.getenv('PORT'),
     }
 }
 
