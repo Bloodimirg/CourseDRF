3ee16f75460845debaa0582a181c645cf6954cbd
Bloodimirg <bloodimirg@gmail.com>
2024-09-13 09:03:53 +0300

    разделение курсов на три сериализатора

--- materials/serializers.py
+++ materials/serializers.py
@@ -1,4 +1,4 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 
 from materials.models import Course, Lesson
 
@@ -8,8 +8,26 @@ class CourseSerializer(ModelSerializer):
         model = Course
         fields = "__all__"
 
+class CourseWithCountSerializer(serializers.ModelSerializer):
+    """Вывод курсов с количеством уроков"""
+    count_lessons = serializers.SerializerMethodField()
 
-class LessonSerializer(ModelSerializer):
+    def get_count_lessons(self, course):
+        return course.lesson_set.count()
+
     class Meta:
-        model = Lesson
-        fields = "__all__"
+        model = Course
+        fields = ("id", "title", "description", "count_lessons")
+
+
+class CourseDetailSerializer(serializers.ModelSerializer):
+    """Вывод курсов с количеством уроков и информацией по всем урокам"""
+    count_lessons = serializers.SerializerMethodField()
+    lessons = LessonSerializer(source='lesson_set', many=True, read_only=True)
+
+    def get_count_lessons(self, course):
+        return course.lesson_set.count()
+
+    class Meta:
+        model = Course
+        fields = ("id", "title", "description", "count_lessons", "lessons")
