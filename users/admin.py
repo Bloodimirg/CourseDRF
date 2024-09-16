5d0db2d2dc33e0577e4c52f0ca27ecf551f913b5
Bloodimirg <bloodimirg@gmail.com>
2024-09-17 01:18:17 +0300

    Добавлена админка пользователей

--- users/admin.py
+++ users/admin.py
@@ -6,7 +6,12 @@ from users.models import User, Payment
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     """Админка пользователя"""
-    list_display = ("id", "email", "phone", "country")
+    list_display = ("id", "email", "phone", "country", "get_groups")
+
+    def get_groups(self, obj):
+        return ", ".join([group.name for group in obj.groups.all()])
+
+    get_groups.short_description = 'Groups'
 
 @admin.register(Payment)
 class PaymentAdmin(admin.ModelAdmin):
