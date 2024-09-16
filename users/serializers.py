44dc14d8ed665c70c612348db32abe999358c74e
Bloodimirg <bloodimirg@gmail.com>
2024-09-16 20:35:23 +0300

    написан Serializer for User

--- users/serializers.py
+++ users/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from users.models import Payment
+from users.models import Payment, User
 
 
 class PaymentSerializer(serializers.ModelSerializer):
@@ -8,4 +8,10 @@ class PaymentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Payment
         fields = "__all__"
+
+class UserSerializer(serializers.ModelSerializer):
+    """Сериализатор пользователя"""
+    class Meta:
+        model = User
+        fields = ("id", "email", "phone", "country", "avatar", "password")
 
