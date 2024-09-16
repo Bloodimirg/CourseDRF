f13ec46e177025ccf56509ab616215a520726fde
Bloodimirg <bloodimirg@gmail.com>
2024-09-16 20:39:29 +0300

    добавлен UserCreateApi

--- users/views.py
+++ users/views.py
@@ -7,9 +7,20 @@ from rest_framework.generics import (
     RetrieveAPIView,
     UpdateAPIView,
 )
+from rest_framework.permissions import AllowAny
+from users.models import Payment, User
+from users.serializers import PaymentSerializer, UserSerializer
 
-from users.models import Payment
-from users.serializers import PaymentSerializer
+
+class UserCreateApiView(CreateAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+    permission_classes = (AllowAny,)
+
+    def perform_create(self, serializer):
+        user = serializer.save(is_active=True)
+        user.set_password(user.password)
+        user.save()
 
 
 class PaymentCreateApiView(CreateAPIView):
