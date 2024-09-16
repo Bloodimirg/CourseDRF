cd2b3dc80d70f602b66a99e4b3f136fb1620b362
Bloodimirg <bloodimirg@gmail.com>
2024-09-17 01:19:02 +0300

    Добавлены привязки к владельцам курсов и уроков

--- materials/models.py
+++ materials/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from config import settings
 
 NULLABLE = {"blank": True, "null": True}
 
@@ -8,10 +9,15 @@ class Course(models.Model):
     title = models.CharField(max_length=50, verbose_name="Название курса")
     image = models.ImageField(upload_to="materials/course", **NULLABLE)
     description = models.TextField(verbose_name="Описание курса", help_text="Введите описание курса", **NULLABLE)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, **NULLABLE, verbose_name='Автор', help_text='Укажите автора')
 
     class Meta:
         verbose_name = "Курс"
         verbose_name_plural = "Курсы"
+        permissions = [
+            ('can_view_course', 'Может просматривать курсы'),
+            ('can_edit_course', 'Может редактировать курсы'),
+        ]
 
     def __str__(self):
         return self.title
@@ -25,6 +31,7 @@ class Lesson(models.Model):
     description = models.TextField(verbose_name="Описание урока", help_text="Введите описание урока", **NULLABLE)
     video_url = models.URLField(**NULLABLE, verbose_name="Ссылка на видео")
     course = models.ForeignKey(Course, on_delete=models.SET_NULL, verbose_name="Название курса", **NULLABLE)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, **NULLABLE, verbose_name="Автор урока")
 
     class Meta:
         verbose_name = "Урок"
