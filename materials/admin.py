9646ba44dc0408e52fffdd285b8d80dd04c54c38
Bloodimirg <bloodimirg@gmail.com>
2024-09-17 01:17:45 +0300

    добавлены поля владельца

--- materials/admin.py
+++ materials/admin.py
@@ -6,10 +6,10 @@ from materials.models import Course, Lesson
 @admin.register(Lesson)
 class LessonAdmin(admin.ModelAdmin):
     """Админка уроков"""
-    list_display = ("pk", "title", "course", "description", "video_url")
+    list_display = ("pk", "title", "course", "description", "video_url", "owner")
 
 
 @admin.register(Course)
 class CourseAdmin(admin.ModelAdmin):
     """Админка курсов"""
-    list_display = ("pk", "title", "image", "description")
+    list_display = ("pk", "title", "image", "description", "owner")
