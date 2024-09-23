08b5a6966f7f08c607307ec3a3d36d5c2aafe743
Bloodimirg <bloodimirg@gmail.com>
2024-09-24 00:11:08 +0300

    написан APIView подписки

--- materials/views.py
+++ materials/views.py
@@ -3,15 +3,17 @@ from rest_framework.generics import (
     DestroyAPIView,
     ListAPIView,
     RetrieveAPIView,
-    UpdateAPIView,
+    UpdateAPIView, get_object_or_404,
 )
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
 
-from materials.models import Course, Lesson
+from materials.models import Course, Lesson, Subscription
 from materials.serializers import (
     CourseSerializer,
-    LessonSerializer,
+    LessonSerializer, SubscriptionSerializer,
 )
 from users.permissions import IsModerator, IsOwner
 
@@ -87,4 +89,21 @@ class LessonDestroyApiView(DestroyAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
     permission_classes = (IsAuthenticated, IsOwner | ~IsModerator)
+
+class SubscriptionViewSet(APIView):
+    serializer_class = SubscriptionSerializer
+
+    def post(self, *args, **kwargs):
+        user = self.request.user
+        course_id = self.request.data.get("course_id")
+        course = get_object_or_404(Course, pk=course_id)
+        sub_item = Subscription.objects.all().filter(user=user).filter(course=course)
+
+        if sub_item.exists():
+            sub_item.delete()
+            message = "Подписка отключена"
+        else:
+            Subscription.objects.create(user=user, course=course)
+            message = "Подписка включена"
+        return Response({"message": message})
 
